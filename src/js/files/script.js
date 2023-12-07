0e191d47f2fc67a774e04adf7b231207cf92c15b
IgorBulakh <bulach.igor@gmail.com>
2023-12-07 17:18:11 +0200

    before after block js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -7,6 +7,7 @@ const beforeAfter = document.querySelector(".before-after");
 
 if (beforeAfter) {
   const beforeAfterArrow = document.querySelector(".before-after__arrow");
+  const afterItem = document.querySelector(".before-after__item--after");
 
   beforeAfter.addEventListener("mouseover", function (e) {
     const targetElement = e.target;
@@ -24,5 +25,38 @@ if (beforeAfter) {
   beforeAfter.addEventListener("mouseleave", function () {
     beforeAfterArrow.classList.remove("before-after__arrow--left");
     beforeAfterArrow.classList.remove("before-after__arrow--right");
+  });
+
+  beforeAfterArrow.addEventListener("mousedown", function (e) {
+    const beforeAfterSize = {
+      with: beforeAfter.offsetWidth,
+      left: beforeAfter.getBoundingClientRect().left - scrollX,
+    };
+
+    function beforeAfterArrowMove(e) {
+      const posLeft = e.clientX - beforeAfterSize.left;
+
+      if (posLeft <= beforeAfterSize.with && posLeft > 0) {
+        const way = 100 - (posLeft / beforeAfterSize.with) * 100;
+
+        beforeAfterArrow.style.cssText = `left: ${posLeft}px`;
+        afterItem.style.cssText = `width: ${way}%`;
+      }
+    }
+    // beforeAfterArrow.addEventListener("dragstart", function (e) {
+    //   e.preventDefault();
+    // });
+
+    document.addEventListener("mousemove", beforeAfterArrowMove);
+    document.addEventListener(
+      "mouseup",
+      function (e) {
+        document.removeEventListener("mousemove", beforeAfterArrowMove);
+        // beforeAfterArrow.removeEventListener("dragstart", function (e) {
+        //   e.preventDefault();
+        // });
+      },
+      { once: true }
+    );
   });
 }
