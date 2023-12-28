966c08ec5121c5d4e1a7a70bdbb6e1c80ce092fc
IgorBulakh <bulach.igor@gmail.com>
2023-12-28 22:13:56 +0200

    first page

--- src/js/files/gallery.js
+++ src/js/files/gallery.js
@@ -1,4 +1,3 @@
-
 /*
 Документація по роботі у шаблоні: https://www.lightgalleryjs.com/docs/
 Документація плагіна: https://www.lightgalleryjs.com/docs/
@@ -11,7 +10,7 @@ import { isMobile, FLS } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 // Підключення базового набору функціоналу
-import lightGallery from 'lightgallery';
+import lightGallery from "lightgallery";
 
 // Плагіни
 // lgZoom, lgAutoplay, lgComment, lgFullscreen, lgHash, lgPager, lgRotate, lgShare, lgThumbnail, lgVideo, lgMediumZoom
@@ -19,42 +18,37 @@ import lightGallery from 'lightgallery';
 //import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.min.js'
 
 // Базові стилі
-import '@scss/libs/gallery/lightgallery.scss';
+import "@scss/libs/gallery/lightgallery.scss";
 // Стилі доповнень
-// import '@scss/libs/gallery/lg-thumbnail.scss';
-// import '@scss/libs/gallery/lg-video.scss';
-// import '@scss/libs/gallery/lg-autoplay.scss';
-// import '@scss/libs/gallery/lg-zoom.scss';
-// import '@scss/libs/gallery/lg-pager.scss';
-// import '@scss/libs/gallery/lg-fullscreen.scss';
-// import '@scss/libs/gallery/lg-share.scss';
-// import '@scss/libs/gallery/lg-comments.scss';s
-// import '@scss/libs/gallery/lg-rotate.scss';
-// import '@scss/libs/gallery/lg-medium-zoom.scss';
-// import '@scss/libs/gallery/lg-relative-caption.scss';
+// import "@scss/libs/gallery/lg-thumbnail.scss";
+// import "@scss/libs/gallery/lg-video.scss";
+// import "@scss/libs/gallery/lg-autoplay.scss";
+// import "@scss/libs/gallery/lg-zoom.scss";
+// import "@scss/libs/gallery/lg-pager.scss";
+// import "@scss/libs/gallery/lg-fullscreen.scss";
+// import "@scss/libs/gallery/lg-share.scss";
+// import "@scss/libs/gallery/lg-comments.scss";
+// import "@scss/libs/gallery/lg-rotate.scss";
+// import "@scss/libs/gallery/lg-medium-zoom.scss";
+// import "@scss/libs/gallery/lg-relative-caption.scss";
 
 // Усі стилі
-// import '@scss/libs/gallery/lightgallery-bundle.scss';
+// import "@scss/libs/gallery/lightgallery-bundle.scss";
 
 // Запуск
-const galleries = document.querySelectorAll('[data-gallery]');
+const galleries = document.querySelectorAll("[data-gallery]");
 if (galleries.length) {
-	let galleyItems = [];
-	galleries.forEach(gallery => {
-		galleyItems.push({
-			gallery,
-			galleryClass: lightGallery(gallery, {
-				// plugins: [lgZoom, lgThumbnail],
-				licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
-				speed: 500,
-			})
-		})
-	});
-	// Додаємо в об'єкт модулів
-	flsModules.gallery = galleyItems;
+  let galleyItems = [];
+  galleries.forEach((gallery) => {
+    galleyItems.push({
+      gallery,
+      galleryClass: lightGallery(gallery, {
+        // plugins: [lgZoom, lgThumbnail],
+        licenseKey: "7EC452A9-0CFD441C-BD984C7C-17C8456E",
+        speed: 500,
+      }),
+    });
+  });
+  // Додаємо в об'єкт модулів
+  flsModules.gallery = galleyItems;
 }
-
-
-
-
-
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -70,3 +70,134 @@ if (beforeAfter) {
     );
   });
 }
+
+const doctor = document.querySelector(".doctors");
+
+if (doctor) {
+  loadDoctor();
+}
+
+async function loadDoctor() {
+  const response = await fetch("files/data/doctors.json", {
+    method: "GET",
+  });
+  if (response.ok) {
+    const responseResult = await response.json();
+    initDoctors(responseResult);
+  } else {
+    alert("Помилка");
+  }
+}
+
+const doctorsList = document.querySelector(".body-doctors__list");
+const doctorsPhoto = document.querySelector(".body-doctors__main-picture");
+const doctorsSkills = document.querySelector(".info-service__line");
+const doctorsStat = document.querySelector(".info-service__stat");
+const doctorsGallery = document.querySelector(".doctors__gallery");
+const doctorLIstActiveElement = `_active`;
+
+function initDoctors(data) {
+  let doctorLIstElement = ``;
+
+  data.doctors.forEach((doctor) => {
+    const isActive = doctor.isActive;
+
+    if (isActive) {
+      buildDoctor(doctor);
+    }
+
+    // lists
+    doctorLIstElement += `
+      <a data-id="${doctor.id}" class="list-doctor__item link-box ${
+        doctor.isActive ? doctorLIstActiveElement : null
+      }" href="#">
+        <div class="link-box__image-ibg">
+          <img src="img/${doctor.media.avatar}" alt="portrait doctor">
+        </div>
+        <div class="link-box__body">
+          <div class="link-box__name">${doctor.name}</div>
+          <div class="link-box__text">
+            ${doctor.position}
+          </div>
+        </div>
+      </a>
+    `;
+  });
+  doctorsList.innerHTML = doctorLIstElement;
+
+  document.addEventListener("click", doctorListActions);
+  function doctorListActions(e) {
+    const targetElement = e.target;
+
+    if (targetElement.closest(".link-box")) {
+      const currentItem = targetElement.closest(".link-box");
+      const doctorId = +currentItem.dataset.id;
+
+      const activeListItem = document.querySelector(".link-box._active");
+      activeListItem.classList.remove("_active");
+      currentItem.classList.add("_active");
+
+      const activeDoctorArray = data.doctors.filter(
+        (item) => item.id === doctorId
+      );
+      const activeDoctor = activeDoctorArray[0];
+      buildDoctor(activeDoctor);
+      e.preventDefault();
+    }
+  }
+}
+
+function buildDoctor(activeDoctor) {
+  const mainPhoto = activeDoctor.media.main;
+  const skills = activeDoctor.skils;
+  const stats = activeDoctor.stat;
+  const gallerySmall = activeDoctor.media.gallery.small;
+  const galleryBig = activeDoctor.media.gallery.big;
+
+  let skillItems = ``;
+  let statItems = ``;
+  let galleryItems = ``;
+
+  // main photo
+  mainPhoto
+    ? (doctorsPhoto.innerHTML = `<img src ="img/${mainPhoto}" alt="portraits doctor">`)
+    : null;
+
+  // skills
+  for (const item in skills) {
+    skillItems += `
+        <div class="skill-service__item">
+          <div style="min-width: ${skills[item]}%" class=" skill-service__info">
+            <div class="skill-service__label">${item}</div>
+            <div class="skill-service__value">${skills[item]}%</div>
+          </div>
+          <div style="width: ${skills[item]}%;" class="skill-service__line skill-service__line--green"></div>
+        </div>`;
+  }
+
+  doctorsSkills.innerHTML = skillItems;
+
+  // stats
+  for (const item in stats) {
+    statItems += `
+        <li class="statistic__item">
+          <div class="statistic__value">${stats[item]}</div>
+          <div class="statistic__label">${item}</div>
+        </li>`;
+  }
+
+  doctorsStat.innerHTML = statItems;
+
+  // gallery
+
+  gallerySmall.forEach((picture, index) => {
+    galleryItems += `
+            <a class="prof-gallery__item-ibg" href="img/${galleryBig[index]}">
+              <img src="img/${picture}" alt="marker bubs">
+            </a>
+          `;
+  });
+
+  doctorsGallery.innerHTML = galleryItems;
+  flsModules.gallery[0].galleryClass.refresh();
+}
